c08669c8a87d22ff384d31228e84e77caf8f948b
Arturo Barba <arturo.barba@iesdonana.org>
2018-10-29 19:09:20 +0100

    Sentencias preparadas con marcadores

--- index.php
+++ index.php
@@ -4,27 +4,55 @@
         <meta charset="utf-8">
         <title>Bases de datos</title>
     </head>
-    <?php
-    $con = new PDO('pgsql:host=localhost;dbname=fa', 'fa', 'fa'); //conexion
-    $numFilas = $con->exec("INSERT INTO genero (genero)
-                            VALUES ('Costumbrismo')");
-    $st = $con->query('SELECT * FROM generos'); //sentencia
-    ?>
-    <style>
-    th, td {border-bottom: 1px solid #ddd;}
-    </style>
-    <table style="margin:auto">
-        <thead>
-            <th>Id</th>
-            <th>Género</th>
-        </thead>
-        <tbody>
-    <?php foreach ($st as $fila): ?>
-         <tr>
-             <td><?= $fila['id']; ?></td>
-             <td><?= $fila['genero']; ?></td>
-         </tr>
-     <?php endforeach; ?>
- </tbody>
-</table>
+    <body>
+        <?php
+        //Mantener titutlo
+        $buscarTitulo = isset($_GET['buscarTitulo'])
+                        ? trim($_GET['buscarTitulo']) : '';
+
+        $con = new PDO('pgsql:host=localhost;dbname=fa', 'fa', 'fa'); //conexion
+        $st = $con->prepare("SELECT *
+                            FROM peliculas
+                            WHERE titulo ILIKE :titulo"); //sentencia
+        $st->execute([':titulo'=> "%$buscarTitulo%"]);
+        ?>
+        <div class="">
+            <fieldset>
+                <legend>Buscar...</legend>
+                <form action="" method="get">
+                    <label for="buscarTitulo">Buscar for titutlo: </label>
+                    <input id="buscarTitulo" type="text" name="buscarTitulo"
+                    value="<?= $buscarTitulo ?>">
+                    <input type="submit" value="Buscar">
+                </form>
+            </fieldset>
+        </div>
+        <div style="margin-top: 20px">
+            <style>
+            th, td {border-bottom: 1px solid #ddd;}
+            </style>
+            <table style="margin:auto">
+                <thead>
+                    <th>Id</th>
+                    <th>Titulo</th>
+                    <th>Año</th>
+                    <th>Sinopsis</th>
+                    <th>Duracion</th>
+                    <th>Género id</th>
+                </thead>
+                <tbody>
+            <?php foreach ($st as $fila): ?>
+                 <tr>
+                     <td><?= $fila['id']; ?></td>
+                     <td><?= $fila['titulo']; ?></td>
+                     <td><?= $fila['anyo']; ?></td>
+                     <td><?= $fila['sinopsis']; ?></td>
+                     <td><?= $fila['duracion']; ?></td>
+                     <td><?= $fila['genero_id']; ?></td>
+                 </tr>
+             <?php endforeach; ?>
+            </tbody>
+            </table>
+        </div>
+    </body>
 </html>
